a0b09cb6ff165f3b1992ad7f6d6264a1b8496469
Travis CI <85788613+Ekt0re@users.noreply.github.com>
2025-11-22 07:12:44 +0100

    upload

--- fester/build/web/flutter_service_worker.js
+++ fester/build/web/flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "9fd21e8bb01332e42b1c9110803c4a77",
+"flutter_bootstrap.js": "cda6e27ac9bc6dbed9732492c53ced6a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "c03fc54f8e227e6d0c0b99846dabc14a",
 "/": "c03fc54f8e227e6d0c0b99846dabc14a",
-"main.dart.js": "7502b62b02f554f7cd02a763f86a9e99",
+"main.dart.js": "c4be8fd53093f21cebc4496722e1f3ab",
 "manifest.json": "1e595278b4aa6c5da5b20ff54acc525f",
 "version.json": "a8f0a59d3b475a5461fc5bf8ad053624"};
 // The application shell files that are downloaded before a service worker can
